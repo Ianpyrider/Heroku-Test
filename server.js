4167ac3e1bedd28a031cd6f670006edb32d4ad0a
Ianpyrider <ianpyrider@gmail.com>
2015-08-13 01:31:11 +0000

    Something Created

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
+var port = process.env.port || 8080
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(port, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+//https://github.com/Ianpyrider/Heroku-Test.git
